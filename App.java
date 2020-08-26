dff101ba9165001abc9a0249672551732be36e12
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-08-26 08:29:01 -0700

    Enter

--- App.java
+++ App.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class App {
     // boo!
     public static void main(String[] args) throws Exception {
@@ -19,10 +21,39 @@ public class App {
         return answer;
     }
 
-    public static void mode(int[] array, int initCounter, int initIndex) {
-        int counter = initCounter;
-        int contra = 1;
-        int i = initIndex;
+    public static void mode(final int[] array) throws Exception {
+        int prime = 0;
+        int sec = 0;
+        int primeCounter = 1;
+        int secCounter = 1;
+        int relay = -1;
+        // List<Integer> modas = new ArrayList<Integer>();
+        for (int i = 0; i < array.length - 2; i++) {
+            prime = (primeCounter < secCounter) ? sec : prime;
+            if (secCounter > primeCounter) {
+                prime = sec;
+                primeCounter = secCounter;
+                relay *= -1;
+                secCounter = 1;
+            }
+
+            switch (relay) {
+                case 1:
+                    if (array[i] == array[i + 1]) {
+                        prime = array[i];
+                        primeCounter++;
+                    } else
+                        relay = -1;
+                    break;
+                case -1:
+                    if (array[i] == array[i + 1]) {
+                        sec = array[i];
+                        secCounter++;
+                    } else
+                        relay = 1;
+                    break;
+                default:
+                    throw new Exception("OOPS!");
 
         for (int j = i; j < array.length; j++) {
             if (array[j] == array[j+1]) {
